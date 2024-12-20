acb04920523c2e4d504a29bcd4f0e7d1447e84e0
Imran Emon <imran.emon@welldev.io>
2024-12-21 03:31:28 +0600

    refactor basic things

--- IronCodingChallenge/MySolution.cs
+++ IronCodingChallenge/MySolution.cs
@@ -8,7 +8,7 @@ namespace IronCodingChallenge
 {
     public class MySolution
     {
-        public static readonly Dictionary<char, string> KeyMap = new Dictionary<char, string>
+        private static readonly Dictionary<char, string> KeyMap = new Dictionary<char, string>
             {
                 { '2', "ABC" },
                 { '3', "DEF" },
@@ -19,86 +19,99 @@ namespace IronCodingChallenge
                 { '8', "TUV" },
                 { '9', "WXYZ" }
             };
+        private const char EndInputChar = '#';
+        private const char BackspaceChar = '*';
+        private const char SpaceChar = ' ';
 
         private static bool ValidateInput(string input)
         {
-            if (string.IsNullOrWhiteSpace(input)) 
+            if (string.IsNullOrWhiteSpace(input))
                 return false;
-            if (input.Last() != '#')
+            if (input.Last() != EndInputChar)
                 return false;
-            if(input.Contains('1')  || input.Contains("0"))
-                return false;
-            // TODO: check if it contains anything else than the predefined characters
-            // then return false;
-
+            // Check if it contains anything else than the predefined characters
+            foreach (char c in input)
+            {
+                if (!KeyMap.ContainsKey(c)
+                    && c != BackspaceChar
+                    && c != SpaceChar
+                    && c != EndInputChar)
+                    return false;
+            }
             return true;
         }
 
         public static string ConvertInput(string input)
         {
-            var isValidInput = ValidateInput(input);
-            if (!isValidInput)
-            {
+            if (!ValidateInput(input))
                 return string.Empty;
-            }
 
-            Stack<char> stack = new Stack<char>();
+            var stack = new Stack<char>();
 
             foreach (char c in input)
             {
-                if (c == '*')
+                switch (c)
                 {
-                    processNumber(stack);
-                    stack.Pop(); // remove the last letter
-                }
-                else if (c == ' ')
-                {
-                    processNumber(stack); // process the last number's corresponding letter
-                }
-                else if (c == '#')
-                {
-                    processNumber(stack); // process the last number's corresponding letter and then return
-                    break;
-                }
-                else
-                {
-                    if(stack.Any() && c != stack.Peek())
+                    case (BackspaceChar):
                         processNumber(stack);
+                        if (stack.Any())
+                            stack.Pop(); // remove the last inserted letter
 
-                    stack.Push(c);
+                        break;
+                    case (SpaceChar):
+                        processNumber(stack); // process the last number's corresponding letter
+                        break;
+
+                    case (EndInputChar):
+                        processNumber(stack); // process the last number's corresponding letter and return
+                        return prepareResult(stack);
+
+                    default:
+                        if (stack.Any() && c != stack.Peek())
+                            processNumber(stack);
+
+                        stack.Push(c);
+                        break;
                 }
             }
 
+            return string.Empty;
+        }
+
+        private static string prepareResult(Stack<char> stack)
+        {
             // build the result from the stack.
             var result = "";
             foreach (char c in stack)
-            {
                 result = c + result;
-            }
+
             return result;
         }
 
         private static void processNumber(Stack<char> stack)
         {
-            char currentChar = stack.Peek();
-            if (!(currentChar >= '2' && currentChar <= '9'))
+            if (!stack.Any())
                 return;
+
+            char currentChar = stack.Peek();
+            if (!KeyMap.ContainsKey(currentChar))
+                return;
+            
             var numberOfTimesAppeared = 0;
             while (stack.Any() && stack.Peek() == currentChar)
             {
                 stack.Pop();
                 numberOfTimesAppeared++;
             }
+            
             char key = currentChar;
             int count = numberOfTimesAppeared;
-
             if (MySolution.KeyMap.ContainsKey(key))
             {
                 string letters = MySolution.KeyMap[key];
                 int index = (count - 1) % letters.Length; // Cycle through letters
                 stack.Push(letters[index]);
             }
-
         }
     }
 }
