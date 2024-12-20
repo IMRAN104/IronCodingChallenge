00b03cc5688d0c1da6e6657e3fa04792408d0ab3
Imran Emon <imran.emon@welldev.io>
2024-12-21 02:49:38 +0600

    Add project files.

---
+++ IronCodingChallenge/MySolution.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronCodingChallenge
+{
+    public class MySolution
+    {
+        public static readonly Dictionary<char, string> KeyMap = new Dictionary<char, string>
+            {
+                { '2', "ABC" },
+                { '3', "DEF" },
+                { '4', "GHI" },
+                { '5', "JKL" },
+                { '6', "MNO" },
+                { '7', "PQRS" },
+                { '8', "TUV" },
+                { '9', "WXYZ" }
+            };
+
+        private static bool ValidateInput(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) 
+                return false;
+            if (input.Last() != '#')
+                return false;
+            if(input.Contains('1')  || input.Contains("0"))
+                return false;
+            // TODO: check if it contains anything else than the predefined characters
+            // then return false;
+
+            return true;
+        }
+
+        public static string ConvertInput(string input)
+        {
+            var isValidInput = ValidateInput(input);
+            if (!isValidInput)
+            {
+                return string.Empty;
+            }
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                if (c == '*')
+                {
+                    processNumber(stack);
+                    stack.Pop(); // remove the last letter
+                }
+                else if (c == ' ')
+                {
+                    processNumber(stack); // process the last number's corresponding letter
+                }
+                else if (c == '#')
+                {
+                    processNumber(stack); // process the last number's corresponding letter and then return
+                    break;
+                }
+                else
+                {
+                    if(stack.Any() && c != stack.Peek())
+                        processNumber(stack);
+
+                    stack.Push(c);
+                }
+            }
+
+            // build the result from the stack.
+            var result = "";
+            foreach (char c in stack)
+            {
+                result = c + result;
+            }
+            return result;
+        }
+
+        private static void processNumber(Stack<char> stack)
+        {
+            char currentChar = stack.Peek();
+            if (!(currentChar >= '2' && currentChar <= '9'))
+                return;
+            var numberOfTimesAppeared = 0;
+            while (stack.Any() && stack.Peek() == currentChar)
+            {
+                stack.Pop();
+                numberOfTimesAppeared++;
+            }
+            char key = currentChar;
+            int count = numberOfTimesAppeared;
+
+            if (MySolution.KeyMap.ContainsKey(key))
+            {
+                string letters = MySolution.KeyMap[key];
+                int index = (count - 1) % letters.Length; // Cycle through letters
+                stack.Push(letters[index]);
+            }
+
+        }
+    }
+}
---
+++ IronCodingChallenge/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronCodingChallenge;
+
+public class OldPhonePad_without_DesignPattern
+{
+    // Mapping of keys to their corresponding letters
+    private static readonly Dictionary<char, string> KeyMap
+        = new Dictionary<char, string>
+        {
+                { '2', "ABC" },
+                { '3', "DEF" },
+                { '4', "GHI" },
+                { '5', "JKL" },
+                { '6', "MNO" },
+                { '7', "PQRS" },
+                { '8', "TUV" },
+                { '9', "WXYZ" }
+        };
+
+    public static string ConvertInput(string input)
+    {
+        StringBuilder output = new StringBuilder();
+        StringBuilder currentSequence = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            if (c == '#') // End of input
+            {
+                break;
+            }
+            else if (c == '*') // Backspace
+            {
+                if (currentSequence.Length > 0)
+                {
+                    // Process the current sequence before backspacing
+                    ProcessCurrentSequence(currentSequence, output);
+                }
+                if (output.Length > 0)
+                {
+                    output.Length--; // Remove last character from output
+                }
+            }
+            else if (c == ' ') // Space indicates a pause
+            {
+                ProcessCurrentSequence(currentSequence, output);
+            }
+            else // Handle key presses
+            {
+                if (currentSequence.Length > 0 && currentSequence[0] != c)
+                {
+                    ProcessCurrentSequence(currentSequence, output);
+                }
+                currentSequence.Append(c);
+            }
+        }
+
+        // Process any remaining sequence
+        ProcessCurrentSequence(currentSequence, output);
+
+        return output.ToString();
+    }
+
+    private static void ProcessCurrentSequence(StringBuilder currentSequence, StringBuilder output)
+    {
+        if (currentSequence.Length > 0)
+        {
+            char key = currentSequence[0];
+            int count = currentSequence.Length;
+
+            if (KeyMap.ContainsKey(key))
+            {
+                string letters = KeyMap[key];
+                int index = (count - 1) % letters.Length; // Cycle through letters
+                output.Append(letters[index]);
+            }
+
+            currentSequence.Clear(); // Clear the current sequence after processing
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("****************************************************");
+        Console.WriteLine(MySolution.ConvertInput("33#")); // Output: E
+        Console.WriteLine(MySolution.ConvertInput("227*#")); // Output: B
+        Console.WriteLine(MySolution.ConvertInput("4433555 555666#")); // Output: HELLO
+        Console.WriteLine(MySolution.ConvertInput("8 88777444666*664#")); // Output: TURING
+        Console.WriteLine(MySolution.ConvertInput("222 2 22#")); // Output: CAB 
+    }
+}
