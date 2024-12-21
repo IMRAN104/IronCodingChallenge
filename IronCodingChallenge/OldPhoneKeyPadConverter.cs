69f0e7e7f09f658484209f83e5e18e5720c28d34
Imran Emon <imran.emon@welldev.io>
2024-12-21 13:08:53 +0600

    further refactor and remove redundant logic

--- IronCodingChallenge/OldPhoneKeyPadConverter.cs
+++ IronCodingChallenge/OldPhoneKeyPadConverter.cs
@@ -1,77 +1,65 @@
-﻿namespace IronCodingChallenge
+﻿namespace IronCodingChallenge;
+public class OldPhoneKeyPadConverter
 {
-    public class OldPhoneKeyPadConverter
+    public static string ConvertOldPhoneInputToRegularText(string input)
     {
-        public static string ConvertInput(string input)
-        {
-            if (!InputValidator.ValidateInput(input))
-                return string.Empty;
-
-            var stack = new Stack<char>();
-
-            foreach (char c in input)
-            {
-                switch (c)
-                {
-                    case (Constants.BackspaceChar):
-                        processLastSequence(stack);
-                        if (stack.Any())
-                            stack.Pop();                            // remove the last inserted letter
-
-                        break;
-                    case (Constants.SpaceChar):
-                        processLastSequence(stack);                 // process the last number's corresponding letter
-                        break;
-
-                    case (Constants.EndInputChar):
-                        processLastSequence(stack);                 // process the last number's corresponding letter and return
-                        return prepareResult(stack);
-
-                    default:
-                        if (stack.Any() && c != stack.Peek())       // if the current character and stacks top differ, then process the last sequence
-                            processLastSequence(stack);
-                        
-                        stack.Push(c);
-                        break;
-                }
-            }
-
+        if (!InputValidator.ValidateInput(input))
             return string.Empty;
-        }
 
-        private static string prepareResult(Stack<char> stack)
+        var stack = new Stack<char>();
+
+        foreach (char c in input)
         {
-            var result = "";
-            foreach (char c in stack)
-                result = c + result;
-
-            return result;
-        }
-
-        private static void processLastSequence(Stack<char> stack)
-        {
-            if (!stack.Any())
-                return;
-
-            char currentChar = stack.Peek();
-            if (!Constants.KeyMap.ContainsKey(currentChar))
-                return;
-            
-            var numberOfTimesAppeared = 0;
-            while (stack.Any() && stack.Peek() == currentChar)
+            switch (c)
             {
-                stack.Pop();
-                numberOfTimesAppeared++;
-            }
-            
-            char key = currentChar;
-            int count = numberOfTimesAppeared;
-            if (Constants.KeyMap.ContainsKey(key))
-            {
-                string letters = Constants.KeyMap[key];
-                int index = (count - 1) % letters.Length;           // Cycle through letters
-                stack.Push(letters[index]);
+                case (Constants.BackspaceChar):
+                    processLastSequence(stack);
+                    if (stack.Any())
+                        stack.Pop();
+                    break;
+
+                case (Constants.SpaceChar):
+                    processLastSequence(stack);
+                    break;
+
+                case (Constants.EndInputChar):
+                    processLastSequence(stack);
+                    return prepareResult(stack);
+
+                default:
+                    if (stack.Any() && c != stack.Peek())       // if the current character and stacks top differ, then process the last sequence
+                        processLastSequence(stack);
+
+                    stack.Push(c);
+                    break;
             }
         }
+
+        return string.Empty;
+    }
+
+    private static string prepareResult(Stack<char> stack)
+        => new string(stack.Reverse().ToArray());
+
+    private static void processLastSequence(Stack<char> stack)
+    {
+        if (!stack.Any())
+            return;
+
+        char currentChar = stack.Peek();
+        if (!Constants.KeyMap.ContainsKey(currentChar))
+            return;
+
+        var currentCharAppearedCount = 0;
+        while (stack.Any() && stack.Peek() == currentChar)
+        {
+            stack.Pop();
+            currentCharAppearedCount++;
+        }
+
+        string letters = Constants.KeyMap[currentChar];
+        int index = (currentCharAppearedCount - 1) % letters.Length;           // Cycle through letters
+        stack.Push(letters[index]);
+        return;
     }
 }
