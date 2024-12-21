d784a50748dc021c13084c890360f2ef3fa8339f
Imran Emon <imran.emon@welldev.io>
2024-12-21 12:24:17 +0600

    Separate the Validator & constants to new classes

---
+++ IronCodingChallenge/InputValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace IronCodingChallenge;
+public class InputValidator
+{
+    public static bool ValidateInput(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return false;
+        if (input.Last() != Constants.EndInputChar)
+            return false;
+        // Check if it contains anything else than the predefined characters
+        foreach (char c in input)
+        {
+            if (!Constants.KeyMap.ContainsKey(c)
+                && c != Constants.BackspaceChar
+                && c != Constants.SpaceChar
+                && c != Constants.EndInputChar)
+                return false;
+        }
+
+        return true;
+    }
+}
--- IronCodingChallenge/Program.cs
+++ IronCodingChallenge/Program.cs
@@ -5,14 +5,14 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("****************************************************");
-        Console.WriteLine(MySolution.ConvertInput("33#")); // Output: E
-        Console.WriteLine(MySolution.ConvertInput("227*#")); // Output: B
-        Console.WriteLine(MySolution.ConvertInput("4433555 555666#")); // Output: HELLO
-        Console.WriteLine(MySolution.ConvertInput("8 88777444666*664#")); // Output: TURING
-        Console.WriteLine(MySolution.ConvertInput("222 2 22#")); // Output: CAB 
-        Console.WriteLine(MySolution.ConvertInput("2#")); // Output: CAB 
-        Console.WriteLine(MySolution.ConvertInput("9#")); // Output: CAB 
-        Console.WriteLine(MySolution.ConvertInput("*#")); // Output: CAB 
-        Console.WriteLine(MySolution.ConvertInput("gff")); // Output: CAB 
+        Console.WriteLine(OldPhoneKeyPadConverter.ConvertInput("33#")); // Output: E
+        Console.WriteLine(OldPhoneKeyPadConverter.ConvertInput("227*#")); // Output: B
+        Console.WriteLine(OldPhoneKeyPadConverter.ConvertInput("4433555 555666#")); // Output: HELLO
+        Console.WriteLine(OldPhoneKeyPadConverter.ConvertInput("8 88777444666*664#")); // Output: TURING
+        Console.WriteLine(OldPhoneKeyPadConverter.ConvertInput("222 2 22#")); // Output: CAB 
+        Console.WriteLine(OldPhoneKeyPadConverter.ConvertInput("2#")); // Output: CAB 
+        Console.WriteLine(OldPhoneKeyPadConverter.ConvertInput("9#")); // Output: CAB 
+        Console.WriteLine(OldPhoneKeyPadConverter.ConvertInput("*#")); // Output: CAB 
+        Console.WriteLine(OldPhoneKeyPadConverter.ConvertInput("gff")); // Output: CAB 
     }
 }
